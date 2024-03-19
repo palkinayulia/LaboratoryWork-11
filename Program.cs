b4d2adbbcfc4869d863b35259ace262ce5c21ddd
palkinayulia <158473956+palkinayulia@users.noreply.github.com>
2024-03-20 00:52:47 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,300 @@
+﻿using ClassLibrary10Lab;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics;
+namespace Лабораторная_работа__11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Watch> queue = new Queue<Watch>();
+            for (int i = 0; i < 5; i++)
+            {
+                Watch c = new Watch();
+                c.RandomInit();
+                queue.Enqueue(c);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                AnalogWatch c2 = new AnalogWatch();
+                c2.RandomInit();
+                queue.Enqueue(c2);
+            }
+            bool number1 = true;
+            do
+            {
+                Console.WriteLine("Меню:" +
+                    "\n1.Добавить объект" +
+                    "\n2.Удалить первый объект" +
+                    "\n3.Запрос количество пульсов" +
+                    "\n4.Запрос количество часов" +
+                    "\n5.Запрос печать всех уник часов" +
+                    "\n6.Вывести все объекты" +
+                    "\n7.Выполнить клонирование коллекции" +
+                    "\n8.Сортировка по бренду по алфавиту" +
+                    "\n9.Поиск объекта" +
+                    "\n10.Завершить");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("Введите элемент для добавления:");
+                        Watch watch1 = new Watch();
+                        watch1.Init();
+                        queue.Enqueue(new Watch(watch1.Brand, watch1.YearIssue, 1));
+                        break;
+                    case "2":
+                        if (queue.Count == 0)
+                        {
+                            Console.WriteLine("Коллекция пуста");
+                            break;
+                        }
+                        else
+                        {
+                            queue.Dequeue();                       //удаляет самый первый объект в очереди
+                            break;
+                        }
+                    case "3":
+                        int count = 0;
+                        foreach (var item in queue)
+                        {
+                            if (item is Watch)
+                                count++;
+                        }
+                        Console.WriteLine($"Количество = {count}");
+                        Console.Write("Чтобы продолжить нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "4":
+                        int count2 = 0;
+                        foreach (var item in queue)
+                        {
+                            if (item is Watch)
+                            {
+                                if (((Watch)item).Brand == "NoBrand")
+                                {
+                                    count2++;
+                                }
+                            }
+                        }
+                        Console.WriteLine($"Количество = {count2}");
+                        Console.Write("Чтобы продолжить нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "5":
+                        foreach (var item in queue)
+                        {
+                            if (item is Watch)
+                            {
+                                ((AnalogWatch)item).Show();
+                                Console.WriteLine("");
+                            }
+                        }
+                        Console.WriteLine("");
+                        Console.Write("Чтобы продолжить нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "6":
+                        foreach (var item in queue)
+                        {
+                            item.Show();
+                            Console.WriteLine("");
+                        }
+                        Console.WriteLine("");
+                        Console.Write("Чтобы продолжить нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "7":
+                        Queue clonQueue = new Queue(queue);                                   //копия
+                        Console.WriteLine($"Количество ссылок в клоне: {clonQueue.Count}");
+                        Console.WriteLine("");
+                        Console.Write("Чтобы продолжить нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "8":
+                        Watch[] SortedArray = new Watch[queue.Count];
+                        int index = 0;
+                        foreach (var item in queue)
+                        {
+                            SortedArray[index] = item;
+                            index++;
+                        }
+                        Array.Sort(SortedArray);
+                        foreach (Watch item in SortedArray)
+                        {
+                            item.Show();
+                            Console.WriteLine();
+                        }
+                        Console.WriteLine("");
+                        Console.Write("Чтобы продолжить нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "9":
+                        Console.Write("Введите номер объекта: ");
+                        int key_obj = int.Parse(Console.ReadLine());
+                        Console.WriteLine("");
+                        queue.ToArray()[key_obj - 1].Show();               //очередь превращает в массив
+                        Console.WriteLine("");
+                        Console.Write("Чтобы продолжить нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "10":
+                        number1 = false;
+                        break;
+                    default:
+                        break;
+                }
+            } while (number1);
+
+
+            Hashtable ht = new Hashtable();
+            Console.WriteLine($"В словаре {ht.Count} элементов");
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    AnalogWatch c = new AnalogWatch();
+                    c.RandomInit();
+                    Watch w = new Watch(c.Brand, c.YearIssue, 1);
+                    ht.Add(w, c);
+                }
+                catch (Exception e) { i--; }
+            }
+            Console.WriteLine("DICTIONARY");
+            bool number2 = true;             
+            do
+            {
+                Console.WriteLine("Меню:" +
+                    "\n1.Добавить объекты" +
+                    "\n2.Удалить объекты" +
+                    "\n3.Запросить кол" +
+                    "\n4.Запросить количество" +
+                    "\n5.Запросить печать всех watch" +
+                    "\n6.Вывести все объекты коллекции на печать" +
+                    "\n7.Выполнить клонирование коллекции" +
+                    "\n8.Сортировка по бренду по алфавиту" +
+                    "\n9.Поиск объекта" +
+                    "\n10.Завершить");
+                switch (Console.ReadLine())
+                {
+                    case "1":                                       //первое действие          
+                        Console.Write("Укажите ключ объекта: ");
+                        string key = Console.ReadLine();
+                        Console.WriteLine("Введите элемент для добавления:");
+                        Watch watch1 = new Watch();
+                        watch1.Init();
+                        ht.Add(key, new Watch(watch1.Brand, watch1.YearIssue, 1));
+                        break;
+                    case "2":                                           //второе действие
+                        if (ht.Count == 0)                       //если в коллекции нет пар ключа и значения
+                        {
+                            Console.WriteLine("Коллекция пуста");
+                            break;
+                        }
+                        else
+                        {
+                            Console.Write("Укажите ключ: ");
+                            key = Console.ReadLine();
+                            ht.Remove(key);                      //удаление пары по ключу
+                            break;
+                        }
+                    case "3":                                           //третье действие
+                        int count = 0;
+                        foreach (var item in ht.Values)          //для всех значений в коллекции значений
+                        {
+                            if (item is AnalogWatch)                           //относится ли к analog
+                                count++;
+                        }
+                        Console.WriteLine($"Количество = {count}");
+                        Console.Write("Чтобы продолжить нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "4":                                            //четвёртое действие
+                        int count2 = 0;
+                        foreach (var item in ht.Values)
+                        {
+                            if (item is Watch)
+                            {
+                                if (((Watch)item).Brand == "NoBrand")
+                                {
+                                    count2++;
+                                }
+                            }
+                        }
+                        Console.WriteLine($"Количество watch = {count2}");
+                        Console.Write("Чтобы продолжить нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "5":                                            //пятое дейсвтвие
+                        foreach (var item in ht.Values)
+                        {
+                            if (item is Watch)
+                            {
+                                ((Watch)item).Show();
+                                Console.WriteLine("");
+                            }
+                        }
+                        Console.WriteLine("");
+                        Console.Write("Чтобы продолжить нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "6":                                            //шестое действие
+                        foreach (var item in ht.Values)
+                        {
+                            ((Watch)item).Show();
+                            Console.WriteLine("");
+                        }
+                        Console.WriteLine("");
+                        Console.Write("Чтобы продолжить нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "7":                                           //седьмое действие
+                        Hashtable clon = (Hashtable)ht.Clone();  //клонирование, присваивает ссылку
+                        Console.WriteLine($"Количество ссылок в клоне: {clon.Count}");
+                        Console.WriteLine("");
+                        Console.Write("Чтобы продолжить нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "8":                                           //восьмое действие
+                        Watch[] SortedArray = new Watch[ht.Count];
+                        int index = 0;
+                        foreach (var item in ht.Values)
+                        {
+                            SortedArray[index] = (Watch)item;
+                            index++;
+                        }
+                        Array.Sort(SortedArray);
+                        foreach (Watch item in SortedArray)
+                        {
+                            item.Show();
+                            Console.WriteLine();
+                        }
+                        Console.WriteLine("");
+                        Console.Write("Чтобы продолжить нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "9":                                           //девятое действие
+                        Console.Write("Введите ключ объекта: ");
+                        string key_obj = Console.ReadLine();
+                        Console.WriteLine("");
+                        ((Watch)ht[key_obj]).Show();     //поиск по ключу и вывод
+                        Console.WriteLine("");
+                        Console.Write("Чтобы продолжить нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "10":                                          //десятое действие
+                        number2 = false;
+                        break;
+                    default:                                            //если выбрано не 1-10
+                        break;
+                }
+            } while (number2);
+
+            TestCollections a = new TestCollections();
+        }
+
+    }
+}
---
+++ TestCollections.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using ClassLibrary10Lab;
+
+namespace Лабораторная_работа__11
+{
+    internal class TestCollections
+    {
+        Stack<Watch> st1;               //Стэк, последним пришёл, первым вышел, доступен только первый
+        Stack<string> st2;
+        Dictionary<Watch, AnalogWatch> dic1;   //Словарь - ассоциативный массив, универсальный тип в отличие от hashtable
+        Dictionary<string, AnalogWatch> dic2;
+        Stopwatch sw = new Stopwatch();     //считает сколько времени выполняется
+        Watch first;
+        AnalogWatch firstV;
+        Watch middle;
+        AnalogWatch middleV;
+        Watch end;
+        AnalogWatch endV;
+        Watch outof = new Watch();
+        AnalogWatch outofV = new AnalogWatch();
+
+        public TestCollections()
+        {
+            st1 = new Stack<Watch>(1000);   //в () указан размер
+            st2 = new Stack<string>(1000);
+            dic1 = new Dictionary<Watch, AnalogWatch>(1000);
+            dic2 = new Dictionary<string, AnalogWatch>(1000);
+            for (int i = 0; i < 1000; i++)
+            {
+                AnalogWatch t = new AnalogWatch();
+                t.RandomInit();
+                st1.Push(t.GetBase);            //вставить в стэк
+                st2.Push(t.GetBase.ToString());
+                try
+                {
+                    dic1.Add(t.GetBase, t);
+                    dic2.Add(t.GetBase.ToString(), t);
+                }
+                catch
+                {
+
+                }
+                if (i == 0)
+                {
+                    first = t.GetBase;
+                    firstV = t.GetTest;
+                }
+                if (i == 500)
+                {
+                    middle = t.GetBase;
+                    middleV = t.GetTest;
+                }
+                if (i == 999)
+                {
+                    end = t.GetBase;
+                    endV = t.GetTest;
+                }
+            }
+            Menu();
+        }
+        private void Menu()
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Выберите действие:" +
+                    "\n1.Добавить элемент в коллекцию" +
+                    "\n2.Удалить элемент из коллекции" +
+                    "\n3.Посмотреть все ключи" +
+                    "\n4.Посмотреть затраченное время на поиск" +
+                    "\n5.Выход");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("Введите эелмент:");
+                        AnalogWatch w = new AnalogWatch();
+                        w.Init();
+                        Watch bb = w.GetBase;
+                        string aa = bb.ToString();
+                        st1.Push(bb);
+                        st2.Push(aa);
+                        try
+                        {
+                            dic1.Add(bb, w);       //добавить в словарь
+                            dic2.Add(aa, w);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Такой элемент уже находится в коллекции!");
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("Введите эелмент:");
+                        w = new AnalogWatch();
+                        w.Init();
+                        int index = 0;
+                        var b = (dic1.Values).ToArray();    //присваем коллекцию значений в массив
+                        foreach (var item in b)
+                        {
+                            if ((w.Brand == b[index].Brand) && (w.YearIssue == b[index].YearIssue))
+                            {
+                                b[index] = null;
+                            }
+                            index++;
+                        }
+                        index = 0;
+                        st1.Clear();        //очищение стэков и словарей
+                        st2.Clear();
+                        dic1.Clear();
+                        dic2.Clear();
+                        foreach (var item in b)
+                        {
+                            if (item != null)
+                            {
+                                Watch new_bb = item.GetBase;
+                                string new_aa = new_bb.ToString();
+                                st1.Push(new_bb);
+                                st2.Push(new_aa);
+                                try
+                                {
+                                    dic1.Add(new_bb, item);
+                                    dic2.Add(new_aa, item);
+                                }
+                                catch
+                                {
+
+                                }
+                            }
+                        }
+                        break;
+                    case "3":
+                        foreach (var item in st2)
+                            Console.WriteLine(item);
+                        Console.Write("Нажмите Enter, чтобы продолжить");
+                        Console.ReadLine();
+                        break;
+                    case "4":
+                        Console.Clear();
+                        Console.WriteLine("Поиск первого элемента");
+                        sw.Start();     //начало таймера
+                        if (st1.Contains(first))    //определяет содержится ли объект в стэке
+                        {
+                            sw.Stop();  //остановка таймера
+                            Console.WriteLine($"Stack<Watch> : {sw.Elapsed}");  //вывод таймера
+                        }
+                        sw.Reset();     //обнуление таймера
+                        sw.Start();
+                        if (st2.Contains(first.ToString())) //определяет содержится ли строка из объекта в стэке
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Stack<string> : {sw.Elapsed}");
+                        }
+                        sw.Reset();
+                        sw.Start();
+                        if (dic1.ContainsKey(first))    //опеределяет содержится ли объект в словаре по ключу
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Dictionary<Watch, AnalogWatch> | ContainsKey : {sw.Elapsed}");
+                        }
+                        sw.Reset();
+                        sw.Start();
+                        if (dic1.ContainsValue(firstV))     //определяет содержится ли объект в словаре по значению
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Dictionary<Watch, AnalogWatch> | ContainsValue : {sw.Elapsed}");
+                        }
+                        sw.Reset();
+                        sw.Start();
+                        if (dic2.ContainsKey(first.ToString()))     //определяет содержится ли строка в словаре по ключу
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Dictionary<string, AnalogWatch> : {sw.Elapsed}");
+                        }
+                        sw.Reset();
+                        Console.WriteLine("");
+                        Console.WriteLine("Поиск средного элемента");
+                        sw.Start();
+                        if (st1.Contains(middle))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Stack<Watch> : {sw.Elapsed}");
+                        }
+                        sw.Reset();
+                        sw.Start();
+                        if (st2.Contains(middle.ToString()))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Stack<string> : {sw.Elapsed}");
+                        }
+                        sw.Reset();
+                        sw.Start();
+                        if (dic1.ContainsKey(middle))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Dictionary<Watch, AnalogWatch> | ContainsKey : {sw.Elapsed}");
+                        }
+                        sw.Reset();
+                        sw.Start();
+                        if (dic1.ContainsValue(middleV))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Dictionary<Watch, AnalogWatch> | ContainsValue : {sw.Elapsed}");
+                        }
+                        sw.Reset();
+                        sw.Start();
+                        if (dic2.ContainsKey(middle.ToString()))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Dictionary<string, AnalogWatch> : {sw.Elapsed}");
+                        }
+                        sw.Reset();
+                        Console.WriteLine("");
+                        Console.WriteLine("Поиск последнего элемента");
+                        sw.Start();
+                        if (st1.Contains(end))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Stack<Watch> : {sw.Elapsed}");
+                        }
+                        sw.Reset();
+                        sw.Start();
+                        if (st2.Contains(end.ToString()))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Stack<string> : {sw.Elapsed}");
+                        }
+                        sw.Reset();
+                        sw.Start();
+                        if (dic1.ContainsKey(end))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Dictionary<Watch, AnalogWatch> | ContainsKey : {sw.Elapsed}");
+                        }
+                        sw.Reset();
+                        sw.Start();
+                        if (dic1.ContainsValue(endV))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Dictionary<Watch, AnalogWatch> | ContainsValue : {sw.Elapsed}");
+                        }
+                        sw.Reset();
+                        sw.Start();
+                        if (dic2.ContainsKey(end.ToString()))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Dictionary<string, Watch> : {sw.Elapsed}");
+                        }
+                        sw.Reset();
+                        Console.WriteLine("");
+                        Console.WriteLine("Поиск элемента не из коллекции");
+                        sw.Start();
+                        if (st1.Contains(outof))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Stack<Watch> : {sw.Elapsed}");
+                        }
+                        else
+                            Console.WriteLine("Не найден!");
+                        sw.Reset();
+                        sw.Start();
+                        if (st2.Contains(outof.ToString()))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Stack<string> : {sw.Elapsed}");
+                        }
+                        else
+                            Console.WriteLine("Не найден!");
+                        sw.Reset();
+                        sw.Start();
+                        if (dic1.ContainsKey(outof))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Dictionary<Watch, AnalogWatch> : {sw.Elapsed}");
+                        }
+                        else
+                            Console.WriteLine("Не найден!");
+                        sw.Reset();
+                        sw.Start();
+                        if (dic1.ContainsValue(outofV))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Dictionary<Watch, AnalogWatch> | ContainsValue : {sw.Elapsed}");
+                        }
+                        else
+                            Console.WriteLine("Не найден!");
+                        sw.Reset();
+                        sw.Start();
+                        if (dic2.ContainsKey(outof.ToString()))
+                        {
+                            sw.Stop();
+                            Console.WriteLine($"Dictionary<string, AnalogWatch> : {sw.Elapsed}");
+                        }
+                        else
+                            Console.WriteLine("Не найден!");
+                        sw.Reset();
+                        Console.WriteLine("");
+                        Console.Write("Чтобы продолжить, нажмите Enter");
+                        Console.ReadLine();
+                        break;
+                    case "5":
+                        Environment.Exit(0);    //прекращение работы программы 
+                        break;
+                }
+            } while (true);
+        }
+    }
+}
